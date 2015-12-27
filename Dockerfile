a857bdd26b561461ceadab7c51b4f20c5626bacd
Cole Brumley <cole.brumley@gmail.com>
2015-12-27 00:17:22 -0600

    Add compose file and add mysql support to runscript

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM    alpine
+FROM    alpine:3.2
 ENV     RDECK_BASE=/opt/rundeck
 ENV     RDECK_JAR=$RDECK_BASE/app.jar
 ENV     PATH=$PATH:$RDECK_BASE/tools/bin
-COPY    run.sh /bin/rundeck
-RUN     apk add --update openjdk7-jre bash && \
+RUN     apk add --update openjdk7-jre bash curl && \
         mkdir -p $RDECK_BASE && \
         wget -O $RDECK_JAR \
             http://dl.bintray.com/rundeck/rundeck-maven/rundeck-launcher-2.6.2.jar && \
-        chmod a+x /bin/rundeck
+        rm -Rf /var/cache/apk/*
+COPY    run.sh /bin/rundeck
 EXPOSE  4440
 VOLUME  /opt/rundeck
 CMD     rundeck
--- run.sh
+++ run.sh
@@ -1,19 +1,72 @@
 #!/bin/bash
 
-trap 'kill $(jobs -p)' EXIT
+# OPTIONS:
+#   DEFAULT_ADMIN_USER          Set the default admin user (defaults to "admin")
+#   DEFAULT_ADMIN_PASSWORD      Set the default admin's password (defaults to "admin")
+#   DEFAULT_USER                Set the default user (defaults to "docker")
+#   DEFAULT_PASSWORD            Set the default user's password (defaults to "docker")
+#   SERVER_MEMORY               Because Java is stupid
+#   SERVER_PORT                 Sets Rundeck's listening port (defaults to "4440")
+#   SERVER_URL                  Sets Rundeck's grails.serverURL
+#   MYSQL_USER                  MySQL username
+#   MYSQL_PASSWORD              MySQL password
+#   MYSQL_ADDR                  MySQL address (defaults to "mysql")
+#   MYSQL_DB                    MySQL database name (defaults to "rundeck")
+#   USE_INTERNAL_IP             When SERVER_URL is undefined, use the container's eth0 address (otherwise try to guess external)
 
+config_properties=$RDECK_BASE/server/config/rundeck-config.properties
 mem=${SERVER_MEMORY:-1024}
 perm=$(($mem/4))
 
+function install_rundeck() {
+    java -Ddefault.user.name=${DEFAULT_USER:-docker} \
+      -Ddefault.user.password=${DEFAULT_PASSWORD:-docker} \
+      -jar $RDECK_JAR --installonly
+
+    sed -i "s|^admin:.*\$|${DEFAULT_ADMIN_USER:-admin}:${DEFAULT_ADMIN_PASSWORD:-admin},user,admin|" \
+      $RDECK_BASE/server/config/realm.properties
+}
+
+function config_grails_url() {
+    # Get eth0's IP
+    if [ -z ${SERVER_URL} ]; then
+        ext_ip=$(curl --silent http://ipv4bot.whatismyipaddress.com)
+        int_ip=$(ip -4 -o addr show scope global eth0 | awk '{gsub(/\/.*/,"",$4); print $4}')
+        if [ -z ${USE_INTERNAL_IP} ]; then
+            SERVER_URL=http://${ext_ip}:${SERVER_PORT:-4440}
+        else
+            SERVER_URL=http://${int_ip}:${SERVER_PORT:-4440}
+        fi
+    fi
+    sed -i "s,^grails\.serverURL.*\$,grails\.serverURL=${SERVER_URL}," $config_properties
+}
+
+function config_mysql() {
+    sql_datasource="dataSource.url = jdbc:mysql://${MYSQL_ADDR:-mysql}/${MYSQL_DB:-rundeck}?autoReconnect=true"
+    sql_un="dataSource.username = ${MYSQL_USER}"
+    sql_pw="dataSource.password = ${MYSQL_PASSWORD}"
+    extra_crap_for_sql="rundeck.projectsStorageType=db\nrundeck.storage.provider.1.type=db\nrundeck.storage.provider.1.path=keys"
+    sed -i "s,^dataSource\.url.*\$,${sql_datasource}," $config_properties
+    echo $sql_un >> $config_properties; echo $sql_pw >> $config_properties
+    echo -e $extra_crap_for_sql >> $config_properties
+}
+
+install_rundeck
+config_grails_url
+
+if ! [ -z ${MYSQL_USER} ] && ! [ -z ${MYSQL_PASSWORD} ]; then
+    config_mysql
+fi
+
+echo "STARTING RUNDECK"
 exec java -Xmx${mem}m \
   -XX:MaxPermSize=${perm}m \
-  -Ddefault.user.name=${DEFAULT_USER:-admin} \
-  -Ddefault.user.password=${DEFAULT_PASSWORD:-admin} \
   -Drundeck.jetty.connector.forwarded=true \
   -Dserver.http.host=0.0.0.0 \
   -Dserver.hostname=$(hostname) \
   -Dserver.http.port=${SERVER_PORT:-4440} \
-  -jar $RDECK_JAR &
+  -jar $RDECK_JAR --skipinstall &
 rd_pid=$!
 
 wait $rd_pid
+echo "RUNDECK HAS DIED :("
